21879408302b409d2b75e71a9bc6a6c2781b2b03
jwatznauer <justinwatznauer@gmail.com>
2023-04-24 17:36:37 -0500

    Login fucntionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,32 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [data, setData] = useState(null);
-  
+    const navigate = useNavigate();
+    //const [data, setData] = useState(null);
+    const { currentUser, login, setError } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // user logged in already,navigate home
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('/api/data/123'); // Replace with your own API endpoint
-          setData(response.data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+      if (currentUser) {
+        navigate("/");
+      }
+    }, [currentUser, navigate]);
   
-      fetchData();
-    }, []);
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       const response = await axios.get('/api/data/123'); // Replace with your own API endpoint
+    //       setData(response.data);
+    //     } catch (error) {
+    //       console.error('Error fetching data:', error);
+    //     }
+    //   };
+  
+    //   fetchData();
+    // }, []);
+
+    async function handleSubmit(e) {
+      e.preventDefault();
+
+      try {
+        setError("");
+        setLoading(true);
+        await login(email, password);
+      }
+      catch(e) {
+        console.log(e);
+        setError('Failed to login');
+      }
+
+      setLoading(false);
+    }
   
     return (
       <div>
-        {data ? (
+        <form onSubmit={handleSubmit}>
+        <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={ loading }>Login</button>
+        {/* {error && <p>{error}</p>} */}
+        </form>
+        {/* {data ? (
           <div>
             <h1>Data:</h1>
             <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
         ) : (
           <p>Loading data...</p>
-        )}
+        )} */}
       </div>
     );
   };
